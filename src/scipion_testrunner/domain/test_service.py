d75497df5f105c09b433e5258121db450892bab7
Martin Salinas <ssalinasmartin@gmail.com>
2024-08-07 10:11:20 +0200

    Type hinting

--- src/scipion_testrunner/domain/test_service.py
+++ src/scipion_testrunner/domain/test_service.py
@@ -1,8 +1,9 @@
 import sys
+from typing import Dict
 
 from ..scipion import test_getter
 
-def run_tests(args):
+def run_tests(args: Dict):
     filtered_lines = test_getter.getAllTests(args['scipion'], args['plugin'], f"tests {args['plugin']}.tests.")
     print(filtered_lines)
     sys.exit(0)
