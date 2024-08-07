7d5cd9308522ec480637a2b5cbdecf93d93c03c0
Martin Salinas <ssalinasmartin@gmail.com>
2024-08-07 11:33:30 +0200

    Rename

--- src/scipion_testrunner/domain/test_service.py
+++ src/scipion_testrunner/domain/test_service.py
@@ -1,10 +1,10 @@
 from typing import Dict
 
 from ..logger import logger
-from ..scipion import test_getter
+from ..scipion import scipion_service
 
 def run_tests(args: Dict):
-    test_list = test_getter.get_all_tests(args['scipion'], args['plugin'])
+    test_list = scipion_service.get_all_tests(args['scipion'], args['plugin'])
     logger(f"FILTERED LINES: {test_list}")
     import sys
     sys.exit(0)
