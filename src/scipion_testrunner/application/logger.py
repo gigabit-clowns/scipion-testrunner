0a76c0e3ea9f8149ee6e7d0cb1e4f9ad9cfb945a
Martin Salinas <ssalinasmartin@gmail.com>
2024-08-07 13:05:28 +0200

    Small fixes

--- src/scipion_testrunner/application/logger.py
+++ src/scipion_testrunner/application/logger.py
@@ -41,23 +41,37 @@ class Logger:
 		"""
 		print(text, flush=True)
 		if self.__log_file is not None:
-			print(__remove_non_printable(text), file=self.__log_file, flush=True)
+			print(self.__remove_non_printable(text), file=self.__log_file, flush=True)
 
 	def log_error(self, text: str, ret_code: int=1):
 		"""
-		### This function prints an error message and stops exection.
+		### Prints an error message and stops exection.
 
 		#### Params:
 		- text (str): Error message to show.
 		- ret_code (int): Optional. Return code to end the exection with.
 		"""
-		self.__call__(red(__remove_non_printable(text)))
+		self.__call__(red(self.__remove_non_printable(text)))
 		sys.exit(ret_code)
+	
+	def __remove_non_printable(self, text: str) -> str:
+		"""
+		### Returns the given text without non printable characters.
+
+		#### Params:
+		- text (str): Text to remove format.
+
+		#### Returns:
+		- (str): Text without format.
+		"""
+		for formatting_char in __FORMATTING_CHARACTERS:
+			text = text.replace(formatting_char, "")
+		return text
 
 ####################### TEXT MODES #######################
 def green(text: str) -> str:
 	"""
-	### This function returns the given text formatted in green color.
+	### Returns the given text formatted in green color.
 
 	#### Params:
 	- text (str): Text to format.
@@ -69,7 +83,7 @@ def green(text: str) -> str:
 
 def yellow(text: str) -> str:
 	"""
-	### This function returns the given text formatted in yellow color.
+	### Returns the given text formatted in yellow color.
 
 	#### Params:
 	- text (str): Text to format.
@@ -81,7 +95,7 @@ def yellow(text: str) -> str:
 
 def red(text: str) -> str:
 	"""
-	### This function returns the given text formatted in red color.
+	### Returns the given text formatted in red color.
 
 	#### Params:
 	- text (str): Text to format.
@@ -93,7 +107,7 @@ def red(text: str) -> str:
 
 def blue(text: str) -> str:
 	"""
-	### This function returns the given text formatted in blue color.
+	### Returns the given text formatted in blue color.
 
 	#### Params:
 	- text (str): Text to format.
@@ -105,7 +119,7 @@ def blue(text: str) -> str:
 
 def bold(text: str) -> str:
 	"""
-	### This function returns the given text formatted in bold.
+	### Returns the given text formatted in bold.
 
 	#### Params:
 	- text (str): Text to format.
@@ -114,19 +128,5 @@ def bold(text: str) -> str:
 	- (str): Text formatted in bold.
 	"""
 	return f"{__BOLD}{text}{__END_FORMAT}"
-
-def __remove_non_printable(text: str) -> str:
-	"""
-	### This function returns the given text without non printable characters.
-
-	#### Params:
-	- text (str): Text to remove format.
-
-	#### Returns:
-	- (str): Text without format.
-	"""
-	for formatting_char in __FORMATTING_CHARACTERS:
-		text = text.replace(formatting_char, "")
-	return text
 
 logger = Logger()
