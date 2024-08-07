023611d167ced07ccf78d397d5e5a61e427e8626
Martin Salinas <ssalinasmartin@gmail.com>
2024-08-07 11:08:07 +0200

    Test: convert command output to test list

--- src/scipion_testrunner/scipion/test_getter.py
+++ src/scipion_testrunner/scipion/test_getter.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from ..shell import shell_service
 from ..logger import logger
 
@@ -15,31 +17,42 @@ def get_all_tests(scipion: str, plugin_module: str):
 	ret_code, output = shell_service.run_shell_command(__get_scipion_test_search_param(scipion, plugin_module))
 	if ret_code:
 		logger.log_error(output)
+
+	test_list = __get_test_list_from_str(output, plugin_module)
 	logger(f"TEST: {output}")
+	logger(f"FILTERED LINES: {test_list}")
 	import sys
 	sys.exit(0)
-
-	# Define test command string variables
-	scipion_tests_starting_spaces = '   '
-
-	# Separate lines into a list
-	lines = output.split('\n')
-
-	# For each line, keep only the ones about individual tests in a reduced form
-	filtered_lines = []
-	for line in lines:
-		if line.startswith(scipion_tests_starting_spaces):
-			filtered_lines.append(line.replace(f'{scipion_tests_starting_spaces}scipion3 tests {plugin_module}.tests.', ''))
 	
 	# If no tests were found, check if module was not found or if plugin has no tests
-	if not filtered_lines:
+	if not test_list:
 		pass
 		# If import caused an error, module was not found
 		#if not testPythonCommand(scipion, f"import {plugin_module}"):
 		#	printFatalError(f"ERROR: No tests were found for module {plugin_module}. Are you sure this module is properly installed?")
 	
 	# Return full list of tests
-	return filtered_lines
+	return test_list
+
+def __get_test_list_from_str(command_text: str, plugin_module: str) -> List[str]:
+	"""
+	### Return the list of tests given a command text
+
+	#### Param:
+	- command_text (str): Command text containing the list of tests inside it
+	- plugin_module (str): Module name of the plugin to obtain tests from
+
+	#### Returns:
+	- (list[str]): List of tests present in the command text
+	"""
+	lines = command_text.split('\n')
+	tests = []
+	leading_chars = __get_test_leading_chars(plugin_module)
+	for line in lines:
+		line = line.lstrip()
+		if line.startswith(leading_chars):
+			tests.append(line.replace(leading_chars, ''))
+	return tests
 
 def __get_scipion_test_search_param(scipion: str, plugin_module: str) -> str:
 	"""
@@ -50,3 +63,15 @@ def __get_scipion_test_search_param(scipion: str, plugin_module: str) -> str:
 	- plugin_module (str): Module name of the plugin to obtain tests from
 	"""
 	return f"{scipion} test --grep {plugin_module}"
+
+def __get_test_leading_chars(plugin_module: str) -> str:
+	"""
+	### Returns the leading characters of every test string
+
+	#### Params:
+	- plugin_module (str): Module name of the plugin to obtain tests from
+
+	#### Returns:
+	- (str): Leading characters of test strings
+	"""
+	return f'scipion3 tests {plugin_module}.tests.'
