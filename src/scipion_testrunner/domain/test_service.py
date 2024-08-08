26ffc74162712864bd10e1bb0bebbc7a152aa0d1
Martin Salinas <ssalinasmartin@gmail.com>
2024-08-08 12:41:41 +0200

    Add dependency test removal

--- src/scipion_testrunner/domain/test_service.py
+++ src/scipion_testrunner/domain/test_service.py
@@ -1,17 +1,17 @@
 from typing import Dict, List
 
 from ..application.logger import logger
-from ..repository import scipion_service, file_service
+from ..repository import scipion_service, file_service, python_service
 
 def run_tests(args: Dict):
     test_list = scipion_service.get_all_tests(args['scipion'], args['plugin'])
     data_sets, skippable_tests = file_service.read_test_data_file(args['testData'])
     logger(f"FULL TESTS: {test_list}")
-    skippable_tests = {'noGpu': test_list[:5]} # TEST
+    skippable_tests = {'gpu': test_list[:5]} # TEST
     test_list = __remove_skippable_tests(test_list, skippable_tests, args['noGpu'])
+    logger(f"SKIPPABLE TESTS: {skippable_tests}")
     logger(f"FULL REMAINING TESTS: {test_list}")
     logger(f"DATA SETS: {data_sets}")
-    logger(f"SKIPPABLE TESTS: {skippable_tests}")
     import sys
     sys.exit(0)
 
@@ -28,6 +28,7 @@ def __remove_skippable_tests(test_list: List[str], skippable_tests: Dict, no_gpu
     - (list[str]): List of tests where skippable ones are removed if applicable
     """
     test_list = __remove_gpu_tests(test_list, skippable_tests.get('gpu', []), no_gpu)
+    test_list = __remove_dependency_tests(test_list, skippable_tests.get('dependencies', []))
     return test_list
 
 def __remove_gpu_tests(test_list: List[str], gpu_test_list: List[str], no_gpu: bool) -> List[str]:
@@ -42,6 +43,42 @@ def __remove_gpu_tests(test_list: List[str], gpu_test_list: List[str], no_gpu: b
     #### Returns:
     - (list[str]): List of tests where GPU-based ones are removed if applicable
     """
-    if no_gpu:
-        return list(set(test_list) - set(gpu_test_list))
+    if not no_gpu:
+        return test_list
+    for gpu_test in gpu_test_list:
+        if gpu_test in test_list:
+            __log_skip_gpu_test(gpu_test)
+            test_list.remove(gpu_test)
     return test_list
+
+def __remove_dependency_tests(test_list: List[str], dependency_test_list: List[Dict]) -> List[str]:
+    """
+    ### Removes all dependency-based tests from the test list if the dependency is not met
+
+    #### Params:
+    - test_list (list[str]): Full list of tests
+    - dependency_test_list (list[dict]): List of dependency-based tests
+
+    #### Returns:
+    - (list[str]): List of tests where dependency-based ones are removed if applicable
+    """
+    for dependency in dependency_test_list:
+        plugin_name = dependency.get('name')
+        module_name = dependency.get('module')
+        if module_name and python_service.exists_python_module(module_name):
+            continue
+        for dependency_test in dependency.get("tests", []):
+            if dependency_test in test_list:
+                __log_skip_dependency_test(dependency_test, plugin_name)
+                test_list.remove(dependency_test)
+    return test_list
+
+def __log_skip_gpu_test(test_name: str):
+    __log_skip_test(test_name, "Needs GPU")
+
+def __log_skip_dependency_test(test_name: str, dependency: str):
+    dependency_name_message = f"with plugin {dependency}" if dependency else ""
+    __log_skip_test(test_name, f"Unmet dependency{dependency_name_message}")
+
+def __log_skip_test(test_name: str, custom_text: str):
+    logger.log_warning(f"Skipping test {test_name}. Reason: {custom_text}.")
