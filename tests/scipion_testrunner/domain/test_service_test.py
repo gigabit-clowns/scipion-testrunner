c5aef1f31116bab2543704ef3ce90d17ffaee97b
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-08-14 22:59:22 +0200

    Extra tests

--- tests/scipion_testrunner/domain/test_service_test.py
+++ tests/scipion_testrunner/domain/test_service_test.py
@@ -8,12 +8,19 @@ __SCIPION_KEY = "scipion"
 __SCIPION = __SCIPION_KEY
 __PLUGIN_KEY = "plugin"
 __PLUGIN = "myplugin"
+__NO_GPU_KEY = "noGpu"
+__TEST_DATA_KEY = "testData"
 __ARGS = {
 	__SCIPION_KEY: __SCIPION,
-	__PLUGIN_KEY: __PLUGIN
+	__PLUGIN_KEY: __PLUGIN,
+	__NO_GPU_KEY: False,
+	__TEST_DATA_KEY: "test.json"
 }
 __DATASETS = ["dataset_1", "dataset_2"]
 __TESTS = [f"test_{i}" for i in range(10)]
+__GPU_KEY = "gpu"
+__DEPENDENCIES_KEY = "dependencies"
+__OTHERS_KEY = "others"
 __SKIPPABLE_GPU = ["test_1", "test_2"]
 __SKIPPABLE_OTHER = [
 	{'test': "test_3", "reason": "Test reason"},
@@ -29,7 +36,7 @@ __INTERNAL_DEPENDENCIES = {
 	"test_9": ["test_10"]
 }
 
-def test_exits_success_when_there_are_not_tests(__mock_get_all_tests, __mock_print):
+def test_exits_success_when_there_are_not_tests(__mock_get_all_tests, __mock_log_warning):
 	__mock_get_all_tests.return_value = []
 	with pytest.raises(SystemExit) as exit_status:
 		test_service.test_scipion_plugin(__ARGS)
@@ -41,14 +48,34 @@ def test_logs_warning_when_there_are_not_tests(__mock_get_all_tests, __mock_log_
 		test_service.test_scipion_plugin(__ARGS)
 	__mock_log_warning.assert_called_once_with(f"Module {__PLUGIN} has not tests. Nothing to run.")
 
+def test_exits_success_when_all_tests_get_removed(
+	__mock_get_all_tests,
+	__mock_read_test_data_file,
+	__mock_remove_skippable_tests,
+	__mock_log_warning
+):
+	__mock_read_test_data_file.return_value = ([], {}, {})
+	__mock_remove_skippable_tests.return_value = []
+	with pytest.raises(SystemExit) as exit_status:
+		test_service.test_scipion_plugin(__ARGS)
+	assert exit_status.value.code == 0
+
+def test_logs_warning_when_all_tests_get_removed(
+	__mock_get_all_tests,
+	__mock_read_test_data_file,
+	__mock_remove_skippable_tests,
+	__mock_log_warning
+):
+	__mock_read_test_data_file.return_value = ([], {}, {})
+	__mock_remove_skippable_tests.return_value = []
+	with pytest.raises(SystemExit) as exit_status:
+		test_service.test_scipion_plugin(__ARGS)
+	__mock_log_warning.assert_called_once_with("There are no tests left. Nothing to run.")
+
 @pytest.fixture
 def __mock_get_all_tests():
 	with patch("scipion_testrunner.repository.scipion_service.get_all_tests") as mock_method:
-		yield mock_method
-
-@pytest.fixture
-def __mock_print():
-	with patch("builtins.print") as mock_method:
+		mock_method.return_value = __TESTS
 		yield mock_method
 
 @pytest.fixture
@@ -59,5 +86,15 @@ def __mock_log_warning():
 @pytest.fixture
 def __mock_read_test_data_file():
 	with patch("scipion_testrunner.repository.file_service.read_test_data_file") as mock_method:
-		mock_method.return_value = (__DATASETS)
+		skippable = {
+			__GPU_KEY: __SKIPPABLE_GPU,
+			__DEPENDENCIES_KEY: __SKIPPABLE_DEPENDENCIES,
+			__OTHERS_KEY: __SKIPPABLE_OTHER
+		}
+		mock_method.return_value = (__DATASETS, skippable, __INTERNAL_DEPENDENCIES)
+		yield mock_method
+
+@pytest.fixture
+def __mock_remove_skippable_tests():
+	with patch("scipion_testrunner.domain.test_service.__remove_skippable_tests") as mock_method:
 		yield mock_method
