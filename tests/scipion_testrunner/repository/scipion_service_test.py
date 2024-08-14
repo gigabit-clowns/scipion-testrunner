06dc7e02cab425a77f8c0f40f1cd680755bed6d9
Martin Salinas <ssalinasmartin@gmail.com>
2024-08-14 16:23:12 +0200

    Fully tested get_all_tests

--- tests/scipion_testrunner/repository/scipion_service_test.py
+++ tests/scipion_testrunner/repository/scipion_service_test.py
@@ -6,7 +6,27 @@ from scipion_testrunner.application.logger import logger
 from scipion_testrunner.repository import scipion_service
 
 __SCIPION = "scipion"
-__MODULE = "mumodule"
+__MODULE = "mymodule"
+__TEST_LIST_STRING = f"""
+TEST: Scipion v3.6.0 - Eugenius
+
+ >>> WARNING: XmippImage library not found!
+  > Please install Xmipp to get full functionality for cryo electron microscopy workflows. Otherwise ignore this.
+(Configuration->Plugins->scipion-em-xmipp -> expand, in Scipion plugin manager window)
+
+
+Set SCIPION_CANCEL_XMIPP_BINDING_WARNING = True to cancel this message.
+
+Scanning tests...
+ scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp_rct
+   scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp_rct.TestXmippRCTWorkflow
+ scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp_ctf_consensus
+   scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp_ctf_consensus.TestCtfConsensus
+ scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp_assignment_tiltpairs
+   scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp_assignment_tiltpairs.TestXmippAssignmentTiltPairsWorkflow
+ scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp
+   scipion3 tests {__MODULE}.tests.workflows.test_workflow_xmipp.TestXmippWorkflow
+"""
 
 def test_exists_with_error_when_test_search_fails(__mock_run_shell_command, __mock_print):
   error_text = "Test fail"
@@ -18,6 +38,28 @@ def test_exists_with_error_when_test_search_fails(__mock_run_shell_command, __mo
     flush=True
   )
 
+def test_exits_with_error_when_plugin_is_not_installed(__mock_run_shell_command, __mock_print, __mock_exists_module):
+  __mock_run_shell_command.return_value = (0, "")
+  __mock_exists_module.return_value = False
+  with pytest.raises(SystemExit):
+    scipion_service.get_all_tests(__SCIPION, __MODULE)
+  __mock_print.assert_called_once_with(
+    logger.red(f"ERROR: No tests were found for module {__MODULE}. Are you sure this module is properly installed?"),
+    flush=True
+  )
+
+def test_returns_expected_test_list(__mock_run_shell_command, __mock_exists_module):
+  __mock_run_shell_command.return_value = (0, __TEST_LIST_STRING)
+  __mock_exists_module.return_value = True
+  assert (
+    scipion_service.get_all_tests(__SCIPION, __MODULE) == [
+      "workflows.test_workflow_xmipp_rct.TestXmippRCTWorkflow",
+      "workflows.test_workflow_xmipp_ctf_consensus.TestCtfConsensus",
+      "workflows.test_workflow_xmipp_assignment_tiltpairs.TestXmippAssignmentTiltPairsWorkflow",
+      "workflows.test_workflow_xmipp.TestXmippWorkflow"
+    ]
+  )
+
 @pytest.fixture
 def __mock_run_shell_command():
   with patch("scipion_testrunner.repository.shell_service.run_shell_command") as mock_method:
@@ -26,4 +68,9 @@ def __mock_run_shell_command():
 @pytest.fixture
 def __mock_print():
   with patch("builtins.print") as mock_method:
+    yield mock_method
+
+@pytest.fixture
+def __mock_exists_module():
+  with patch("scipion_testrunner.repository.python_service.exists_python_module") as mock_method:
     yield mock_method
