e2f0448a89bf3d0b5e1597c1d4519812f7f2f32a
Martin Salinas <ssalinasmartin@gmail.com>
2024-08-14 15:08:17 +0200

    Controlled error messages

--- tests/scipion_testrunner/repository/file_service_test.py
+++ tests/scipion_testrunner/repository/file_service_test.py
@@ -1,9 +1,9 @@
 import json
-from io import BytesIO
 from unittest.mock import patch
 
 import pytest
 
+from scipion_testrunner.application.logger import logger
 from scipion_testrunner.repository import file_service
 
 __DATASETS_KEY = "datasets"
@@ -12,17 +12,18 @@ __TEST_DEPENDENCIES_KEY = "test-dependencies"
 __DUMMY_FILE_PATH = "path"
 __FILE_DATA = {
   __DATASETS_KEY: ['1', '2'],
-  "skippable": {'key': 'value'},
-  "test-dependencies": {'key': 'value'}
+  __SKIPPABLE_KEY: {'key': 'value'},
+  __TEST_DEPENDENCIES_KEY: {'key': 'value'}
 }
-__LOG_FILE = BytesIO(json.dumps(__FILE_DATA).encode())
+__EXCEPTION_TEXT = "Test"
+__JSON_EXCEPTION = json.JSONDecodeError(__EXCEPTION_TEXT, __DUMMY_FILE_PATH, 1)
 
 def test_returns_empty_fields(__mock_log):
   assert (
     file_service.read_test_data_file("") == ([], {}, {})
   ), "Empty file path should have returned empty fields."
 
-def test_returns_expected_fields(__mock_open):
+def test_returns_expected_fields(__mock_open, __mock_json_load):
   assert (
     file_service.read_test_data_file(__DUMMY_FILE_PATH) == (
       __FILE_DATA[__DATASETS_KEY],
@@ -34,22 +35,29 @@ def test_returns_expected_fields(__mock_open):
 def test_exits_on_file_not_found_error(__mock_open_raise_file_not_found_error, __mock_log):
   with pytest.raises(SystemExit):
     file_service.read_test_data_file(__DUMMY_FILE_PATH)
+  __mock_log.assert_called_once_with(logger.red(f"ERROR: File '{__DUMMY_FILE_PATH}' does not exist."))
 
 def test_exits_on_is_a_directory_error(__mock_open_raise_is_directory_error, __mock_log):
   with pytest.raises(SystemExit):
     file_service.read_test_data_file(__DUMMY_FILE_PATH)
+  __mock_log.assert_called_once_with(logger.red(f"ERROR: Path '{__DUMMY_FILE_PATH}' provided is a directory."))
 
 def test_exits_on_permission_error(__mock_open_raise_permission_error, __mock_log):
   with pytest.raises(SystemExit):
     file_service.read_test_data_file(__DUMMY_FILE_PATH)
+  __mock_log.assert_called_once_with(logger.red(f"ERROR: Permission denied to open file '{__DUMMY_FILE_PATH}'."))
 
-def test_exits_on_json_decode_error(__mock_open_wrong_file, __mock_log):
+def test_exits_on_json_decode_error(__mock_open, __mock_json_load_raise_json_decode_error, __mock_log):
   with pytest.raises(SystemExit):
     file_service.read_test_data_file(__DUMMY_FILE_PATH)
+  __mock_log.assert_called_once_with(
+    logger.red(f"ERROR: Invalid JSON format in file '{__DUMMY_FILE_PATH}':\n{__JSON_EXCEPTION}")
+  )
 
 def test_exits_on_exception(__mock_open_raise_exception, __mock_log):
   with pytest.raises(SystemExit):
     file_service.read_test_data_file(__DUMMY_FILE_PATH)
+  __mock_log.assert_called_once_with(logger.red(f"An unexpected error occurred:\n{__EXCEPTION_TEXT}"))
 
 @pytest.fixture
 def __mock_log():
@@ -59,25 +67,30 @@ def __mock_log():
 @pytest.fixture
 def __mock_open():
   with patch("builtins.open") as mock_method:
-    mock_method.return_value = __LOG_FILE
+    yield mock_method
+
+@pytest.fixture
+def __mock_json_load():
+  with patch("json.load") as mock_method:
+    mock_method.return_value = __FILE_DATA
     yield mock_method
 
 @pytest.fixture
 def __mock_open_raise_file_not_found_error(__mock_open):
-  __mock_open.side_effect = FileNotFoundError()
+  __mock_open.side_effect = FileNotFoundError(__EXCEPTION_TEXT)
 
 @pytest.fixture
 def __mock_open_raise_is_directory_error(__mock_open):
-  __mock_open.side_effect = IsADirectoryError()
+  __mock_open.side_effect = IsADirectoryError(__EXCEPTION_TEXT)
 
 @pytest.fixture
 def __mock_open_raise_permission_error(__mock_open):
-  __mock_open.side_effect = PermissionError()
+  __mock_open.side_effect = PermissionError(__EXCEPTION_TEXT)
 
 @pytest.fixture
-def __mock_open_wrong_file(__mock_open):
-  __mock_open.return_value = BytesIO("['a', 'b']".encode())
+def __mock_json_load_raise_json_decode_error(__mock_json_load):
+  __mock_json_load.side_effect = __JSON_EXCEPTION
 
 @pytest.fixture
 def __mock_open_raise_exception(__mock_open):
-  __mock_open.side_effect = Exception()
+  __mock_open.side_effect = Exception(__EXCEPTION_TEXT)
