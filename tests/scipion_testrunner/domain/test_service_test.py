10bff25ab68c49ea19d3e0a213c8a6277f26c83c
MartinSalinas98 <ssalinasmartin@gmail.com>
2024-08-14 22:37:56 +0200

    Test variables

--- tests/scipion_testrunner/domain/test_service_test.py
+++ tests/scipion_testrunner/domain/test_service_test.py
@@ -12,6 +12,22 @@ __ARGS = {
 	__SCIPION_KEY: __SCIPION,
 	__PLUGIN_KEY: __PLUGIN
 }
+__DATASETS = ["dataset_1", "dataset_2"]
+__TESTS = [f"test_{i}" for i in range(10)]
+__SKIPPABLE_GPU = ["test_1", "test_2"]
+__SKIPPABLE_OTHER = [
+	{'test': "test_3", "reason": "Test reason"},
+	{'test': "test_4"}
+]
+__SKIPPABLE_DEPENDENCIES = [
+	{"name": "testplugin", "module": "pluginmodule", "tests": ["test_5"]}
+]
+__INTERNAL_DEPENDENCIES = {
+	"test_1": ["test_6"],
+	"test_7": ["test_8"],
+	"test_8": ["test_7"],
+	"test_9": ["test_10"]
+}
 
 def test_exits_success_when_there_are_not_tests(__mock_get_all_tests, __mock_print):
 	__mock_get_all_tests.return_value = []
@@ -38,4 +54,10 @@ def __mock_print():
 @pytest.fixture
 def __mock_log_warning():
 	with patch("scipion_testrunner.application.logger.Logger.log_warning") as mock_method:
+		yield mock_method
+
+@pytest.fixture
+def __mock_read_test_data_file():
+	with patch("scipion_testrunner.repository.file_service.read_test_data_file") as mock_method:
+		mock_method.return_value = (__DATASETS)
 		yield mock_method
